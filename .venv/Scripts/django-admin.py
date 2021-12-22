8441f10903d6f437b8f072d6c182f40a62c9d02f
Shubham Singh <Shubh1122000@gmail.com>
2021-12-22 21:54:53 +0530

    initial commit

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shubham\onedrive\desktop\django-based-hospital-mangament-system-master\django-based-hospital-mangament-system-master\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
