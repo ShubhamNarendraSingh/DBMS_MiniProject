2957b34e38a5f53e6e4f560b0b1fd4968e424a86
Shubham Singh <57426987+ShubhamNarendraSingh@users.noreply.github.com>
2024-03-15 21:11:51 +0530

    Delete .venv directory

--- .venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\shubham\onedrive\desktop\django-based-hospital-mangament-system-master\django-based-hospital-mangament-system-master\.venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
